ad0a99a55b369f76c6119b7b4a946ab194935d3e
Furkan Ekiz <123004647+Furkann-ekz@users.noreply.github.com>
2024-11-10 17:12:15 +0300

    Add files via upload

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -19,29 +19,33 @@ int main(int ac, char **av)
 	size_t	x;
 	int		i;
 
-	if (ac < 4 && (!av[1] || !av[2] || !av[3]))
+	if (ac < 4)
 		return (cout << "Invalid argument. Please use like './<output> <filename> <string 1> <string 2>'" << endl, 1);
 	i = -1;
 	file = av[1];
 	oldstr = av[2];
 	newstr = av[3];
-
 	ifstream	infile(file);
 	if (!infile.is_open())
-		return (cout << "File cannot be opened.", 1);
+		return (cout << "File cannot be opened." << endl, 1);
 	while (getline(infile, line))
 		temp += line + '\n';
 	infile.close();
+	ofstream	oldfile("oldfile");
+	if (!(oldfile.is_open()))
+		return (cout << "File cannot be create." << endl, 1);
+	oldfile << temp;
+	oldfile.close();
 	x = 0;
-	while ((x = temp.find(oldstr, x)))
+	while ((x = temp.find(oldstr, x)) != string::npos)
 	{
 		temp.replace(x, oldstr.length());
 		x += newstr.length();
 	}
 	ofstream outfile(file);
 	if (!(outfile.is_open()))
-		return (cout << "File cannot be opened.", 1);
+		return (cout << "File cannot be opened." << endl, 1);
 	outfile << temp;
 	outfile.close();
-	cout << "Changed succesfully." << ;
+	cout << "Changed succesfully." << endl;
 }
